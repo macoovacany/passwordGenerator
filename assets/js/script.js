61a9700bb1484e682107a87e75c0babfc888fca6
macoovacany <macoovacany@hotmail.com>
2021-03-20 14:15:27 +1030

    added generate password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,127 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+
+
+// *********************************************** // 
+// Main code for the generate password function
+// *********************************************** // 
+
+// limits on password characters
+const MIN_PASSWORD_CHARS = 8; // 7 no, 8 yes
+const MAX_PASSWORD_CHARS = 128; // 128 yes, 129 no
+
+// character classes
+var charClasses = [
+  {
+    characterClassName: "lowerLetters",
+    promptString: "Do you want lower case characters?",
+    characterArray: 'abcdefghijklmnopqrstuvwxyz'.split('')
+  } ,
+  {
+    characterClassName: "upperLetters",
+    promptString: "Do you want upper case characters?",
+    characterArray: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  } ,
+  {
+    characterClassName: "digits",
+    promptString: "Do you want digigts?",
+    characterArray: '0123456789'.split('')
+  } , 
+  {
+    characterClassName: "special",
+    promptString: "Do you want special characters?",
+    characterArray: ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('')
+  },
+  // maybe later
+  // {  
+  //   characterClassName: "emjoi",
+  //   promptString: "Do you want emojis?",
+  //   characterArray: "😀😃😄😁😆😅😂🤣🥲☺️😊😇🙂🙃😉😌😍🥰😘😗😙😚😋😛" + 
+  //   "😝😜🤪🤨🧐🤓😎🥸🤩🥳😏😒😞😔😟😕🙁☹️😣😖😫😩🥺😢" + 
+  //   "😭😤😠😡🤬🤯😳🥵🥶😱😨😰😥😓🤗🤔🤭🤫🤥😶😐😑😬" + 
+  //   "🙄😯😦😧😮😲🥱😴🤤😪😵🤐🥴🤢🤮🤧😷🤒🤕🤑🤠😈👿" + 
+  //   "👹👺🤡💩👻💀☠️👽👾🤖🎃😺😸😹😻😼😽🙀😿😾".split('')
+  // }
+]  // end of the character classes options to use in generating the password.
+
+
+
+// create the pool of characters to choose from
+// called from inside the generatePassword function
+// assummes that charClasses is available, and mutates the array
+function getCharacterPool(finalCharPool, charClassesArray) {
+
+  // loop through the charClasses array and add the appropriate array of 
+  // characters to the final selection array 
+  charClassesArray.forEach(charClass => {
+    if (confirm(charClass.promptString)) {
+      finalCharPool = finalCharPool.concat(charClass.characterArray)
+    }
+  }) // closes the forEach callback function
+  return finalCharPool;
+} 
+
+
+
+function getNumPasswordChars(){
+  var numChars = NaN;
+  var promptString = `Please enter the number of password characters. \n` + 
+                    `(between ${MIN_PASSWORD_CHARS} and ${MAX_PASSWORD_CHARS})`;
+
+
+  // loop while trying get a valid number from the user
+  numChars = parseInt(prompt(promptString)); //
+  while ((numChars < MIN_PASSWORD_CHARS) || (MAX_PASSWORD_CHARS > 128)){
+    alert(`Number of characters must be between ${MIN_PASSWORD_CHARS} and ${MAX_PASSWORD_CHARS}.`);
+    numChars = parseInt(prompt('Please enter the number of password characters')); // update prompt string
+    }
+  
+  // numChars  
+  return numChars;
+}
+
+// 
+
+
+// generate password 
+function generatePassword(){
+
+  var numChars = getNumPasswordChars();
+  if (isNaN(numChars)){
+     // user pressed cancel on the prompt and getNumPasswordChars return NaN
+     //   --> bail out of this function and return empty string
+    return '';
+  }
+
+
+  var finalCharacterPool = getCharacterPool([], charClasses); 
+
+  // check to see that at least one character class has been selected:
+  while (finalCharacterPool.length === 0){
+    alert('You must choose at least one class of characters for your password');
+    finalCharacterPool = getCharacterPool([], charClasses);
+  }
+
+  //  build the password by selecting a random element from our pool
+  //  for the number of character we have
+  var passwordText ='';
+  for(let i = 0; i<numChars; i++){
+    passwordText = passwordText + finalCharacterPool[Math.floor(Math.random() * finalCharacterPool.length)]
+  }
+  return passwordText;
+};
+
+// *********************************************** // 
+// Interface to the web page
+// *********************************************** // 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
+
